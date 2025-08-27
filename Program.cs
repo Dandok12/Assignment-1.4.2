ce882daebf71f89a515e3e8f4f30d891863ae488
dahoo <dahoo@DESKTOP-SE8G5DJ>
2025-08-27 19:01:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//Create a class student
+
+
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+class Student
+{
+
+    public int studentID { get; set; }
+    public string studentFirstname { get; set; }
+    public string studentLastname { get; set; }
+    public char studentGrade { get; set; }
+
+    class Program
+    {
+        static void Main()
+        {
+            Student student = new Student();
+
+            student.studentID = 111;
+            student.studentFirstname = "Dan";
+            student.studentLastname = "Chung";
+            student.studentGrade = 'A';
+
+            Console.WriteLine("Student Info: ");
+            Console.WriteLine("ID" + student.studentID);
+            Console.WriteLine("First name" + student.studentFirstname);
+            Console.WriteLine("Last name" + student.studentLastname);
+            Console.WriteLine("Grade" + student.studentGrade);
+        }
+    }
+
+}
+            
+
+        
